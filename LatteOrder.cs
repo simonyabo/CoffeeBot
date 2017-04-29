cdb3a94ce4475c2a8247cb7d713169aeb4505347
Simon Haile <Simon Haile>
2017-04-29 15:58:39 -0700

    Configured bot to work with twilio over text.

---
+++ LatteOrder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+#pragma warning disable 649
+
+//The LatteOrder is the simple form you want to fill out. It must be serializable so the bot can be stateless.
+// The order of fields defines the default order in which questions will be asked. 
+// Enumerations shows the legal options for each field in the SandwichOrder and the order is the order values will be presented 
+// in a conversation.
+
+namespace Bot_Application1
+{
+
+    public enum CoffeeOptions
+    {
+        Esspresso, Americano, Latte, Machiatto, Seasonal
+    }
+    public enum SizeOptions
+    {
+        Tall, Grande, Venti
+    };
+    public enum FlavorOptions
+    {
+        Mocha, Caramel, Vanilla, CinnamonDolce, Hazelnut, ToffeeNut, Peppermint, Rasberry, Classic
+    };
+    public enum ExtraShotOptions
+    {
+        One, Two, Three, Four, Five
+    };
+
+    [Serializable]
+    [Template(TemplateUsage.NotUnderstood, "I do not understand \"{0}\".", "Try again, I don't get \"{0}\".")]
+    public class LatteOrder
+    {
+        [Prompt("What kind of {&} would you like?")]
+        public CoffeeOptions Coffee;
+        
+        [Prompt("What {&} would you like? {||}")]
+        public SizeOptions? Size;
+
+        [Optional]
+        [Prompt("How about a {&} or two? {||}",ChoiceFormat ="{1}")]
+        public List<FlavorOptions> Flavor;
+
+        [Optional]
+        public ExtraShotOptions? ExtraShots;
+
+        public static IForm<LatteOrder> BuildForm()
+        {
+            return new FormBuilder<LatteOrder>()
+                .Message("Welcome to the Coffee Order Bot!")
+                .Build();
+        }
+    };
+}
---
+++ Services/TwilioLookupService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Twilio.Lookups;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1.Services
+{
+    public class TwilioLookupService
+    {
+        public static Number GetNumberInfo(string number)
+        {
+            var lookupClient = new LookupsClient("AC1a7661b31b926a5830af3df19e54e8a2", "ee3c03f6799bab89090e148c2d623f38");
+            var response = lookupClient.GetPhoneNumber(number, true);
+
+            return response;
+        }
+    }
+}
